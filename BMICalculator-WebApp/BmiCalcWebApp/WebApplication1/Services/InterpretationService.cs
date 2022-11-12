cf7e176b0d1e02acd4ac6bcb2e50816433dea961
Damon German <damon.german@hyland.com>
2022-11-12 17:43:00 -0600

    made some changes

--- BMICalculator-WebApp/BmiCalcWebApp/WebApplication1/Services/InterpretationService.cs
+++ BMICalculator-WebApp/BmiCalcWebApp/WebApplication1/Services/InterpretationService.cs
@@ -4,13 +4,17 @@
     {
         public Task<string> InterpretBmiAsync(double bmi)
         {
-            return Task.Run(() => bmi switch
+            return Task.Run(() =>
             {
-                < 18.5 => "Under Weight",
-                < 24.9 => "Normal",
-                < 29.9 => "Overweight",
-                > 29.9 => "Obese",
-                _ => string.Empty
+                Thread.Sleep(100);
+                return bmi switch
+                {
+                    < 18.5 => "Under Weight",
+                    < 24.9 => "Normal",
+                    < 29.9 => "Overweight",
+                    > 29.9 => "Obese",
+                    _ => string.Empty
+                };
             });
         }
     }
